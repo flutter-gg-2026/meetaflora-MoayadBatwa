e9646423b921a02ec0eb793746d278275a60f74e
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-04-11 19:24:03 +0300

    set flowers page

--- image_ai/lib/features/flowers/presentation/pages/flowers_feature_screen.dart
+++ image_ai/lib/features/flowers/presentation/pages/flowers_feature_screen.dart
@@ -1,35 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_ai/core/constants/app_colors.dart';
+import 'package:image_ai/core/widgets/loading_widget.dart';
 import 'package:image_ai/features/flowers/presentation/cubit/flowers_cubit.dart';
+import 'package:image_ai/features/flowers/presentation/cubit/flowers_state.dart';
 
 class FlowersFeatureScreen extends StatelessWidget {
   const FlowersFeatureScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    final _ = context.read<FlowersCubit>();
+    final cubit = context.read<FlowersCubit>();
 
     return Scaffold(
-
       appBar: AppBar(title: const Text('Flowers')),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            GridView.builder(
-              primary: false,
-              shrinkWrap: true,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-              ),
-              itemCount: 12,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 4,
-                  color: AppColors.success,
-                  shadowColor: AppColors.error,
-                  clipBehavior: .antiAlias,
-                  child: Image.asset("assets/images/flower_1.jpg", fit: .fill,),
-                );
+            BlocBuilder<FlowersCubit, FlowersState>(
+              builder: (context, state) {
+                switch (state) {
+                  case FlowersInitialState _:
+                    cubit.getFlowersMethod();
+                    return LoadingWidget();
+
+                  case FlowersErrorState _:
+                    return Column(
+                      children: [
+                        Center(
+                          child: Stack(
+                            alignment: .bottomCenter,
+                            children: [
+                              Container(
+                                clipBehavior: .antiAlias,
+                                height: 200,
+                                decoration: BoxDecoration(
+                                  shape: .circle,
+                                  color: AppColors.warning,
+                                ),
+                                child: Image.asset(
+                                  "assets/images/flower_1.jpg",
+                                  fit: .fill,
+                                ),
+                              ),
+
+                              Icon(
+                                Icons.not_interested,
+                                color: AppColors.error,
+                                size: 200,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          state.message,
+                          style: TextStyle(
+                            decoration: .underline,
+                            fontWeight: .bold,
+                            fontSize: 20,
+                            color: AppColors.error,
+                          ),
+                        ),
+                      ],
+                    );
+
+                  case FlowersSuccessState _:
+                    return GridView.builder(
+                      primary: false,
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                      ),
+                      itemCount: state.flowers.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          elevation: 4,
+                          color: AppColors.success,
+                          shadowColor: AppColors.success,
+                          clipBehavior: .antiAlias,
+                          child: Image.asset(
+                            state.flowers[index].path,
+                            fit: .fill,
+                          ),
+                        );
+                      },
+                    );
+
+                  default:
+                    return SizedBox.shrink();
+                }
               },
             ),
           ],
