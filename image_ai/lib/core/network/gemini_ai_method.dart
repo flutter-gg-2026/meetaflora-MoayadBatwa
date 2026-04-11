d0e2992d80f3d81a58ae64a29c332b45d9d49350
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-04-12 01:19:40 +0300

    not working - does not accept downloaded images in assests folder

--- image_ai/lib/core/network/gemini_ai_method.dart
+++ image_ai/lib/core/network/gemini_ai_method.dart
@@ -6,11 +6,9 @@ import 'package:image_ai/core/network/api_endpoints.dart';
 import 'package:image_ai/core/network/dio_client.dart';
 
 extension GeminiMethods on DioClient {
-  Future<String> uploadImage(String imagePath) async {
+  Future<String> uploadImage(String base64Image) async {
     // Convert image into base64<String> datatype as ai-api require
-    final file = File(imagePath);
-    final bytes = await file.readAsBytes();
-    final base64Image = base64Encode(bytes);
+    
 
     // create the post method for body/data JSON as ai-api require
     final data = {
--- image_ai/lib/features/ai_response/data/datasources/ai_response_remote_data_source.dart
+++ image_ai/lib/features/ai_response/data/datasources/ai_response_remote_data_source.dart
@@ -1,31 +1,38 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:image_ai/core/network/dio_client.dart';
+import 'package:image_ai/core/network/gemini_ai_method.dart';
 import 'package:injectable/injectable.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:image_ai/core/services/local_keys_service.dart';
 import 'package:image_ai/features/ai_response/data/models/ai_response_model.dart';
 import 'package:image_ai/core/errors/network_exceptions.dart';
 
-
 abstract class BaseAiResponseRemoteDataSource {
   Future<String> getAiResponse({required String imagePath});
 }
 
-
 @LazySingleton(as: BaseAiResponseRemoteDataSource)
 class AiResponseRemoteDataSource implements BaseAiResponseRemoteDataSource {
   final DioClient _dio;
-  
 
-   AiResponseRemoteDataSource(this._dio);
+  AiResponseRemoteDataSource(this._dio);
 
-
-
-    @override
+  @override
   Future<String> getAiResponse({required String imagePath}) async {
     try {
+      log(imagePath);
+      final imageFile = File(imagePath);
+      final bytes = await imageFile.readAsBytes();
+      final base64Image = base64Encode(bytes);
+      print(imageFile);
+      final response = await _dio.uploadImage(base64Image);
+      print("datasourse response: $response");
       return "LLM Response";
     } catch (error) {
-     throw FailureExceptions.getException(error);
+      throw FailureExceptions.getException(error);
     }
   }
 }
