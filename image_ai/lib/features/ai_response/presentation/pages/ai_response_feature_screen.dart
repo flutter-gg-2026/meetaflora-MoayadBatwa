2880478ac7b5272017ce48112aeb0799bae9356e
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-04-12 07:25:34 +0300

    set ai response UI

--- image_ai/lib/features/ai_response/presentation/pages/ai_response_feature_screen.dart
+++ image_ai/lib/features/ai_response/presentation/pages/ai_response_feature_screen.dart
@@ -2,6 +2,7 @@ import 'package:any_image_view/any_image_view.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_ai/core/constants/app_colors.dart';
 import 'package:image_ai/features/ai_response/presentation/cubit/ai_response_cubit.dart';
 import 'package:image_ai/features/ai_response/presentation/cubit/ai_response_state.dart';
 
@@ -46,13 +47,51 @@ class AiResponseFeatureScreen extends StatelessWidget {
                     builder: (context, state) {
                       switch (state) {
                         case AiResponseLoadingState _:
-                          return AnyImageView(imagePath: "assets/lottie/ai_loading.json", height: 150,);
+                          return AnyImageView(
+                            imagePath: "assets/lottie/ai_loading.json",
+                            height: 150,
+                          );
 
                         case AiResponseErrorState _:
                           return Icon(Icons.error);
 
                         case AiResponseSuccessState _:
-                          return Text(state.aiResponse);
+                          return Stack(
+                            clipBehavior: .none,
+                            children: [
+                              Container(
+                                width: 250,
+                                padding: .all(16),
+                                decoration: BoxDecoration(
+                                  borderRadius: .circular(20),
+                                  gradient: LinearGradient(
+                                    begin: .topLeft,
+                                    end: .bottomRight,
+                                    colors: [
+                                      const Color.fromARGB(164, 59, 218, 246),
+                                      const Color.fromARGB(178, 40, 243, 114),
+                                    ],
+                                  ),
+                                  boxShadow: kElevationToShadow[4],
+                                ),
+                                child: Text(
+                                  state.aiResponse,
+                                  style: TextStyle(
+                                    color: AppColors.textPrimary,
+                                  ),
+                                ),
+                              ),
+
+                              Positioned(
+                                top: -30,
+                                right: -30,
+                                child: AnyImageView(
+                                  imagePath: "assets/lottie/ai_response.json",
+                                  height: 100,
+                                ),
+                              ),
+                            ],
+                          );
 
                         default:
                           return SizedBox.shrink();
@@ -63,11 +102,13 @@ class AiResponseFeatureScreen extends StatelessWidget {
                   SizedBox(height: 20),
 
                   FilledButton(
-                    onPressed: () {
-                      cubit.getAiResponseMethod(imageURL);
-                    },
-                    child: Text("Get AI Response"),
-                  ).animate(onPlay: (controller) => controller.repeat()).shimmer(duration: 1.5.seconds),
+                        onPressed: () {
+                          cubit.getAiResponseMethod(imageURL);
+                        },
+                        child: Text("Get AI Response"),
+                      )
+                      .animate(onPlay: (controller) => controller.repeat())
+                      .shimmer(duration: 1.5.seconds),
 
                   SizedBox(height: 400),
                 ],
