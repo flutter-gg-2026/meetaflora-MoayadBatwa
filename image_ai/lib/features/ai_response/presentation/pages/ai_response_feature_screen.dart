bda87ed6dcd0d3bc5eae1a1207e5cbc9a9b3a976
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-04-12 06:40:58 +0300

    switch to AnyImageView & background in ai screen

--- image_ai/lib/features/ai_response/presentation/pages/ai_response_feature_screen.dart
+++ image_ai/lib/features/ai_response/presentation/pages/ai_response_feature_screen.dart
@@ -1,3 +1,4 @@
+import 'package:any_image_view/any_image_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_ai/features/ai_response/presentation/cubit/ai_response_cubit.dart';
@@ -12,40 +13,61 @@ class AiResponseFeatureScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('Ai Response About Your Image')),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Center(
-              child: SizedBox(
-                height: 300,
-                child: Image.network(imageURL, fit: .fitHeight),
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/background.jpg"),
+            fit: .fill,
+          ),
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16, right: 16),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Center(
+                    child: AnyImageView(
+                      imagePath: imageURL,
+                      height: 250,
+                      width: 250,
+                      fit: .fill,
+                      borderRadius: .circular(20),
+                      boxShadow: kElevationToShadow[4],
+                    ),
+                  ),
+            
+                  SizedBox(height: 20),
+            
+                  BlocBuilder<AiResponseCubit, AiResponseState>(
+                    builder: (context, state) {
+                      switch (state) {
+                        case AiResponseErrorState _:
+                          return Icon(Icons.error);
+            
+                        case AiResponseSuccessState _:
+                          return Text(state.aiResponse);
+            
+                        default:
+                          return SizedBox.shrink();
+                      }
+                    },
+                  ),
+
+                  SizedBox(height: 20),
+            
+                  FilledButton(
+                    onPressed: () {
+                      cubit.getAiResponseMethod(imageURL);
+                    },
+                    child: Text("Get AI Response"),
+                  ),
+
+                  SizedBox(height: 400),
+                ],
               ),
             ),
-
-            SizedBox(height: 20),
-
-            BlocBuilder<AiResponseCubit, AiResponseState>(
-              builder: (context, state) {
-                switch (state) {
-                  case AiResponseErrorState _:
-                    return Icon(Icons.error);
-
-                  case AiResponseSuccessState _:
-                    return Text(state.aiResponse);
-
-                  default:
-                    return SizedBox.shrink();
-                }
-              },
-            ),
-
-            FilledButton(
-              onPressed: () {
-                cubit.getAiResponseMethod(imageURL);
-              },
-              child: Text("Get AI Response"),
-            ),
-          ],
+          ),
         ),
       ),
     );
